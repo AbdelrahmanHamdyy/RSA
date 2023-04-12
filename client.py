99b80d7dcf50ca8ce3caef981eb03562ee6a6a55
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-04-12 18:14:54 +0200

    Flush prints & exchange keys

--- client.py
+++ client.py
@@ -8,24 +8,33 @@ clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 def runClient():
     clientSocket.connect((HOST, PORT))
-    print("Client connected.. Start Chatting!")
+    print("Client connected.. Start Chatting!", flush=True)
     numberOfBits = clientSocket.recv(RSA.PACKET_SIZE).decode()
+    print("Client: Number of bits received", flush=True)
     
     publicKey, privateKey = RSA.generateKeys(int(numberOfBits))
+    print("Client: Keys generated")
     turn = 0
     
-    PU = RSA.receivePublicKey(clientSocket)
-    RSA.sendPublicKey(clientSocket, publicKey)
+    print("Client: Receiving PU..", flush=True)
+    e = int(clientSocket.recv(RSA.PACKET_SIZE).decode())
+    n = int(clientSocket.recv(RSA.PACKET_SIZE).decode())
+    PU = (e, n)
+    print("Client: PU received", flush=True)
+    print("Client: Sending PU..", flush=True)
+    clientSocket.send(str(publicKey[0]).encode()) # Send e
+    clientSocket.send(str(publicKey[1]).encode()) # Send n
+    print("Client: PU sent", flush=True)
     
     while True:
-        result = RSA.send(clientSocket, PU) if turn else RSA.receive(clientSocket, privateKey)
+        result = RSA.sendMsg(clientSocket, PU) if turn else RSA.receiveMsg(clientSocket, privateKey)
         if (not result):
             break
         
         turn ^= 1
         
     clientSocket.close()
-    print("Client Side: Connection Closed!")
+    print("Client Side: Connection Closed!", flush=True)
 
 if __name__ == '__main__':
     runClient()
--- server.py
+++ server.py
@@ -9,28 +9,37 @@ serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 def runServer():
     serverSocket.bind((HOST, PORT))
     serverSocket.listen()
-    print(f"Server is listening on {HOST}:{PORT}")
+    print(f"Server is listening on {HOST}:{PORT}", flush=True)
     conn, address = serverSocket.accept()
-    print("Connection from: " + str(address))
+    print("Connection from: " + str(address), flush=True)
     
     numberOfBits = input("Key size (Number of bits): ")
     conn.send(numberOfBits.encode())
+    print("Server: Number of bits sent", flush=True)
     
     publicKey, privateKey = RSA.generateKeys(int(numberOfBits))
+    print("Server: Keys generated")
     turn = 1
     
-    RSA.sendPublicKey(conn, publicKey)
-    PU = RSA.receivePublicKey(conn)
+    print("Server: Sending PU..", flush=True)
+    conn.send(str(publicKey[0]).encode()) # Send e
+    conn.send(str(publicKey[1]).encode()) # Send n
+    print("Server: PU sent", flush=True)
+    print("Server: Receiving PU..", flush=True)
+    e = int(conn.recv(RSA.PACKET_SIZE).decode())
+    n = int(conn.recv(RSA.PACKET_SIZE).decode())
+    PU = (e, n)
+    print("Server: PU received", flush=True)
     
     while True:
-        result = RSA.send(conn, PU) if turn else RSA.receive(conn, privateKey)
+        result = RSA.sendMsg(conn, PU) if turn else RSA.receiveMsg(conn, privateKey)
         if (not result):
             break
         
         turn ^= 1
         
     conn.close()
-    print("Server Side: Connection Closed!")
+    print("Server Side: Connection Closed!", flush=True)
 
 if __name__ == '__main__':
     runServer()
